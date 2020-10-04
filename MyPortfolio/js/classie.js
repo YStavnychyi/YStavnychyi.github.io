1ff9404cd4014f43b18fdd2f22f27eee3f5adb24
YStavnychyi <61946050+YStavnychyi@users.noreply.github.com>
2020-10-04 22:20:44 +0200

    MyPortfolio

--- MyPortfolio/js/classie.js
+++ MyPortfolio/js/classie.js
@@ -1,7 +1,4 @@
 /*!
- * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
- * 
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
@@ -15,7 +12,6 @@
 
 'use strict';
 
-// class helper functions from bonzo https://github.com/ded/bonzo
 
 function classReg( className ) {
   return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
